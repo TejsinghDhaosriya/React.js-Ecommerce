d69ccbb3ff6014cd522bf3c18282a492b805f9c6
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-10-31 08:42:51 +0530

    arch

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './homepage.styles.scss'
 const HomePage = ()=>(
   <div className = 'homepage'>
       <div className='directory-menu'>
@@ -19,18 +19,26 @@ const HomePage = ()=>(
         </div>
         <div className = 'menu-item'>
            <div className ='content'>
-               <h1 className='title'>womens</h1>
+               <h1 className='title'>watch</h1>
+               <h1 className='subtitle'>SHOP NOW</h1>
+           </div>
+        
+        </div>
+        <div className = 'menu-item'>
+           <div className ='content'>
+               <h1 className='title'>mens</h1>
                <h1 className='subtitle'>SHOP NOW</h1>
            </div>
 
         </div>
         <div className = 'menu-item'>
            <div className ='content'>
-               <h1 className='title'>watch</h1>
+               <h1 className='title'>womens</h1>
                <h1 className='subtitle'>SHOP NOW</h1>
            </div>
 
         </div>
+        
       </div>
   </div>
 
